0f298181229d7e19f2f3410a6191724109ee300f
quyanh1189 <quyanh159@gmail.com>
2022-01-23 12:59:42 +0700

    Update DAO as per tutorial

--- app/src/main/java/com/example/musicapp/ArticleDao.java
+++ app/src/main/java/com/example/musicapp/ArticleDao.java
@@ -2,15 +2,24 @@ package com.example.musicapp;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
 
 @Dao
 public interface ArticleDao {
-    @Query("SELECT * FROM article ORDER BY timePosted ASC")
+    @Query("SELECT * FROM article_table ORDER BY timePosted ASC")
     List<Article> getAll();
-    @Query("SELECT * FROM article WHERE id = :id ORDER BY timePosted ASC")
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Article article);
+
+    @Query("DELETE FROM article_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM article_table WHERE id = :id ORDER BY timePosted ASC")
     List<Article> getById(int id);
 }
 
